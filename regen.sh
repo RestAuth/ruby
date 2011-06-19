7b3b72b4870def0899cfe2bcb0fa6c0765bbd268
David Kaufmann <astra@fsinf.at>
2011-06-19 15:02:56 +0200

    buildscript update

--- regen.sh
+++ regen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # remove old things
-rm restauth-0.2.gem
+rm restauth-*.gem
 gem uninstall restauth
 
 # generate new gem
 gem build restauth.gemspec
 
 # install new gem
-gem install --user-install restauth-0.2.gem
+gem install --user-install restauth-*.gem
